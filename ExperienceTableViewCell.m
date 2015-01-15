a20c10e617e18b4820821a0beb727ce73aeabaee
dprasanthig <dprasanthig@users.noreply.github.com>
2015-01-15 10:59:24 -0800

    Create ExperienceTableViewCell.m

---
+++ ExperienceTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  ExperienceTableViewCell.m
+//  Heylets
+//
+//  Created by Prasanthi on 1/13/15.
+//  Copyright (c) 2015 prasanthi. All rights reserved.
+//
+
+#import "ExperienceTableViewCell.h"
+
+@implementation ExperienceTableViewCell
+@synthesize expDescription,personImg,personNameLabel,placeNameLabel;
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
