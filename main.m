ff6c24e9068a53aeae7ca9c223c3d99b80c9c806
dprasanthig <dprasanthig@users.noreply.github.com>
2015-01-15 11:00:11 -0800

    Create main.m

---
+++ main.m
@@ -0,0 +1,17 @@
+
+//
+//  main.m
+//  Heylets
+//
+//  Created by prasanthi on 1/11/15.
+//  Copyright (c) 2015 prasanthi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
