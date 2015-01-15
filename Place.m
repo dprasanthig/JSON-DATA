f8bcc92887303b67dde70260e6800b71ab11e781
dprasanthig <dprasanthig@users.noreply.github.com>
2015-01-15 10:58:09 -0800

    Create Place.m

---
+++ Place.m
@@ -0,0 +1,15 @@
+
+//
+//  Place.m
+//  Heylets
+//
+//  Created by Prasanthi on 1/12/15.
+//  Copyright (c) 2015 prasanthi. All rights reserved.
+//
+
+#import "Place.h"
+
+@implementation Place
+@synthesize city,address,placename;
+
+@end
