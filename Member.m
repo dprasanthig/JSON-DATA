54fc0ef2655d9f2f3ff43e0edea1583109ea8bef
dprasanthig <dprasanthig@users.noreply.github.com>
2015-01-15 10:56:33 -0800

    Create Member.m

---
+++ Member.m
@@ -0,0 +1,14 @@
+//
+//  Member.m
+//  Heylets
+//
+//  Created by Prasanthi on 1/12/15.
+//  Copyright (c) 2015 prasanthi. All rights reserved.
+//
+
+#import "Member.h"
+
+@implementation Member
+@synthesize imgId,memberId,name,personImage;
+
+@end
