68a87b373bcc03e473416ffbaba2c68da5244dff
dprasanthig <dprasanthig@users.noreply.github.com>
2015-01-15 10:55:22 -0800

    Create Experiance.m

---
+++ Experiance.m
@@ -0,0 +1,14 @@
+//
+//  Experiance.m
+//  Heylets
+//
+//  Created by Prasanthi on 1/12/15.
+//  Copyright (c) 2015 prasanthi. All rights reserved.
+//
+
+#import "Experiance.h"
+
+@implementation Experiance
+@synthesize category,memberData,placeData,imageId,description;
+
+@end
